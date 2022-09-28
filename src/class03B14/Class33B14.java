834f0a833b5386577995a64bb2bd39b1d91ac2df
alamkalani <kalanialam@gmail.com>
2022-09-27 20:24:04 -0400

    second commit

--- src/class03B14/Class33B14.java
+++ src/class03B14/Class33B14.java
@@ -5,7 +5,7 @@ public class Class33B14 {
 	public static void main(String[] args) {
 
 		System.out.println("hey this is a git class");
-		System.out.println("");
+		System.out.println("hey this is a change in my project");
 
 	}
 
