4ca3c7d49b85a8f22bf5a10132fe519614552b20
alamkalani <kalanialam@gmail.com>
2022-09-27 20:25:24 -0400

    third commit

--- src/class03B14/Class33B14.java
+++ src/class03B14/Class33B14.java
@@ -6,7 +6,7 @@ public class Class33B14 {
 
 		System.out.println("hey this is a git class");
 		System.out.println("hey this is a change in my project");
-
+		System.out.println("new versin");
 	}
 
 }
